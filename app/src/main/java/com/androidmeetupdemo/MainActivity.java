a63ffdbeb32dd06b1e9fe428c3e472f5a2110a0c
Danko Misic <dankomisic81@gmail.com>
2016-11-22 16:36:59 +0100

    new fragments

--- app/src/main/java/com/androidmeetupdemo/MainActivity.java
+++ app/src/main/java/com/androidmeetupdemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.androidmeetupdemo.search.views.SearchFragment;
+import com.androidmeetupdemo.weather.view.WeatherFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SearchFragment searchFragment = new SearchFragment();
+        WeatherFragment weatherFragment = new WeatherFragment();
         FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction().add(R.id.fragment_container,searchFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.fragment_container,weatherFragment).commit();
     }
 }
--- app/src/main/java/com/androidmeetupdemo/weather/view/WeatherFragment.java
+++ app/src/main/java/com/androidmeetupdemo/weather/view/WeatherFragment.java
@@ -3,11 +3,15 @@ package com.androidmeetupdemo.weather.view;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.androidmeetupdemo.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by danko.misic on 11/18/16.
@@ -15,11 +19,14 @@ import com.androidmeetupdemo.R;
 public class WeatherFragment extends Fragment{
 
 
+    @BindView(R.id.weather_recycler_view)
+    RecyclerView mRecycView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_weather,container,false);
-
+        ButterKnife.bind(this,view);
 
         return view;
     }
