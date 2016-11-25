ef1457fc965103f996bc28792a68e95e60c1cb86
Danko Misic <dankomisic81@gmail.com>
2016-11-25 15:36:10 +0100

    reactive wraper

--- app/src/main/java/com/androidmeetupdemo/weather/model/WeatherDataRequests.java
+++ app/src/main/java/com/androidmeetupdemo/weather/model/WeatherDataRequests.java
@@ -6,20 +6,50 @@ import com.androidmeetupdemo.network.models.TownWeather;
 import java.util.List;
 
 import rx.Observable;
+import rx.functions.Func1;
 
 /**
  * Created by danko.misic on 11/23/16.
  */
 public class WeatherDataRequests {
 
+    public static final String apiKey = "32bc7442fa84ffe869d8e4d9905757e2";
     private WeatherServiceApi serviceApi;
 
     public WeatherDataRequests(WeatherServiceApi weatherApi) {
         serviceApi=weatherApi;
     }
 
+    private Observable<Integer> weatherForTown(String town){
+        return serviceApi.getTownWeather(town,apiKey).map(new Func1<TownWeather, Double>() {
+            @Override
+            public Double call(TownWeather townWeather) {
+                return townWeather.getMain().getTemp();
+            }
+        }).map(new Func1<Double, Integer>() {
+            @Override
+            public Integer call(Double aDouble) {
+                return aDouble.intValue();
+            }
+        });
+    }
 
-    /*public Observable<List<Integer>> listOfWeatherForTowns(List<String> towns){
-         return
-    }*/
+    public Observable<List<Integer>> listOfWeatherForTowns(List<String> towns){
+        return Observable.from(towns)
+                .map(new Func1<String, Observable<Integer>>() {
+                    @Override
+                    public Observable<Integer> call(String s) {
+                        return weatherForTown(s);
+                    }
+                })
+                .toList()
+                .flatMap(new Func1<List<Observable<Integer>>, Observable<List<Integer>>>() {
+                    @Override
+                    public Observable<List<Integer>> call(List<Observable<Integer>> integers) {
+                        return Observable;
+                    }
+                });
+
+
+    }
 }
